a280d54fcdfd7a9f997b3331ded533370b96cf45
egotrippingh <tripplesix@inbox.ru>
2025-10-06 17:57:59 +0300

    skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Header } from './components/Header/Header.jsx'
-import {Pizza} from "./components/Pizza/Pizza.jsx";
-import {Sort} from "./components/Sort/Sort.jsx";
+import { Pizza } from "./components/Pizza/Pizza.jsx";
+import { Sort } from "./components/Sort/Sort.jsx";
+import {PizzaSkeleton} from './components/PizzaSkeleton';
+
 import './App.css'
 
 
 export const App = () => {
 
   const [pizzas, setPizzas] = React.useState([])
-
+  const [isLoading, setIsLoading] = React.useState(true)
 React.useEffect(() => {
   const fetchPizzas = async () => {
     try {
@@ -17,9 +19,11 @@ React.useEffect(() => {
       if (!res.ok) {
         throw new Error ("Ошибка при подключении к серверу")
       }
-     
+      
       const data = await res.json()
+
       setPizzas(data) 
+      setIsLoading(false)
     } catch (err) {
       console.error(err)
     }
@@ -33,12 +37,13 @@ React.useEffect(() => {
         <Header />
         <Sort />
         <div className="pizza-container">
-          {pizzas.map(obj => (<Pizza key={obj.id} {...obj}
-          />))}
+        {
+          isLoading
+            ? [...new Array(6)].map((_, index) => <PizzaSkeleton key={index} />)
+            : pizzas.map((obj) => <Pizza key={obj.id} {...obj} />)
+        }
         </div>
       </div>
-
-
     </>
   )
 }
---
+++ src/components/PizzaSkeleton/index.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+export const PizzaSkeleton = (props) => (
+  <ContentLoader 
+    speed={2}
+    width={366}
+    height={631}
+    viewBox="0 0 366 631"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <circle cx="184" cy="154" r="98" /> 
+    <rect x="87" y="23" rx="10" ry="10" width="191" height="19" /> 
+    <rect x="156" y="282" rx="10" ry="10" width="53" height="19" /> 
+    <rect x="137" y="316" rx="10" ry="10" width="91" height="19" /> 
+    <rect x="152" y="368" rx="10" ry="10" width="61" height="19" /> 
+    <circle cx="128" cy="377" r="20" /> 
+    <rect x="85" y="506" rx="10" ry="10" width="195" height="45" /> 
+    <rect x="19" y="565" rx="10" ry="10" width="327" height="45" /> 
+    <rect x="80" y="432" rx="10" ry="10" width="205" height="45" /> 
+    <circle cx="237" cy="377" r="20" />
+  </ContentLoader>
+)
